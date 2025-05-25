b8e3bcc42cbd9555694fa929579f4d847478f63d
Gonçalo Ferreira <goncalo1133@gmail.com>
2025-05-25 10:48:43 +0100

    fix target controller

--- proj/src/controller/target_controller.c
+++ proj/src/controller/target_controller.c
@@ -14,12 +14,20 @@ extern unsigned v_res;
 Target targets[MAX_ACTIVE_TARGETS]; /**< @brief Array of targets */
 uint8_t target_hits = 0; /**< @brief Number of targets hit */
 
-void (target_controller_init)() {
-    srand(time(NULL)); // Initialize random seed
+void generate_random_position_in_game_area(int16_t *x, int16_t *y) {
+    *x = rand() % 726;                     // 0 a 725 => 726 valores
+    *y = 40 + rand() % (541 - 40);         // 40 a 540 => 501 valores
+}
 
+bool is_overlapping(Target *a, int16_t x, int16_t y, uint16_t width, uint16_t height) {
+    return !(x + width  <= a->x || a->x + a->width  <= x || y + height <= a->y || a->y + a->height <= y);
+}
+
+
+void (target_controller_init)() {
     for (int i = 0; i < MAX_ACTIVE_TARGETS; i++) {
-        int16_t x = rand() % (h_res - 50); // 50 é uma margem de "segurança"
-        int16_t y = rand() % (v_res - 50);
+        int16_t x, y;
+        generate_random_position_in_game_area(&x, &y);
         
         create_target(&targets[i], x, y, (xpm_map_t) target);
     }
@@ -28,10 +36,30 @@ void (target_controller_init)() {
 void (target_controller_update)() {
     for (int i = 0; i < MAX_ACTIVE_TARGETS; i++) {
         if (!targets[i].isVisible) {
-            int16_t x = rand() % (h_res - 50);
-            int16_t y = rand() % (v_res - 50);
+            int16_t x, y;
+            uint16_t width = targets[i].width;
+            uint16_t height = targets[i].height;
+
+            bool valid_position = false;
+            int max_attempts = 100;
+
+            while (!valid_position && max_attempts-- > 0) {
+                generate_random_position_in_game_area(&x, &y);
+                valid_position = true;
+
+                for (int j = 0; j < MAX_ACTIVE_TARGETS; j++) {
+                    if (j == i || !targets[j].isVisible) continue;
+
+                    if (is_overlapping(&targets[j], x, y, width, height)) {
+                        valid_position = false;
+                        break;
+                    }
+                }
+            }
             
-            create_target(&targets[i], x, y, (xpm_map_t) target);
+            if(valid_position){
+                create_target(&targets[i], x, y, (xpm_map_t) target);
+            }
         }
     }
 }
