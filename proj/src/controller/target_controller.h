b1d28c399ca835b1cc19f3d1a0ddf85237b83f92
Pedro <up202306326@up.pt>
2025-05-26 16:06:24 +0000

    mode 2 init

--- proj/src/controller/target_controller.h
+++ proj/src/controller/target_controller.h
@@ -8,11 +8,14 @@
 #define MAX_ACTIVE_TARGETS 3 /**< @brief Maximum number of active targets on the screen */
 
 void (target_controller_init_mode1)();
+void (target_controller_init_mode2)();
 void (target_controller_update_mode1)();
+void (target_controller_update_mode2)();
 void (target_controller_draw)();
 void (target_controller_reset)();
 bool (target_controller_check_hit)(uint16_t x, uint16_t y);
 void generate_random_position_in_game_area(int16_t *x, int16_t *y);
+void generate_random_position_in_game_area_mode2(int16_t *x, int16_t *y)
 bool is_overlapping(Target *a, int16_t x, int16_t y, uint16_t width, uint16_t height);
 
 #endif
